973d026b80f805309143ab67e843ae6045a96bc5
waelbh <47089212+waelbh@users.noreply.github.com>
2020-09-09 23:18:55 +0100

    front end updated

--- src/app/pages/productions/produit/addproduct/addproduct.component.ts
+++ src/app/pages/productions/produit/addproduct/addproduct.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
-import { NbIconLibraries } from '@nebular/theme';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
+import { NbIconLibraries } from "@nebular/theme";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
 @Component({
   selector: "ngx-addproduct",
   templateUrl: "./addproduct.component.html",
   styleUrls: ["./addproduct.component.scss"],
 })
-export class AddproductComponent implements OnInit {
+export class AddproductComponent implements OnInit, OnChanges {
+  ligneRecetteForm: FormGroup;
   familles: any[] = [
     {
       id: 1,
@@ -30,6 +37,14 @@ export class AddproductComponent implements OnInit {
       description: "morceau",
       prodSaison: 150,
       prodSaisonAttein: 60,
+      nbrProd: null,
+    },
+    {
+      id: 4,
+      name: "Gâteau morceau",
+      description: "Gâteau en morceau",
+      prodSaison: 200,
+      prodSaisonAttein: 140,
       nbrProd: null,
     },
   ];
@@ -76,39 +91,83 @@ export class AddproductComponent implements OnInit {
       },
     },
   ];
-  units:any[]=[
-    { value:"NONE",label:"Aucun"},
-    { value:"METER",label:"Par mètre"},
-    { value:"KG",label:"En kilogramme"},
-    { value:"LITER",label:"En litre"},
-    { value:"PIECE",label:"Par piece"},
-    { value:"UNIT",label:"Par Unité"}
-  ]
-  produit: any = { name: "", stocklm: "", SelectedFamily: {}, unit: "" ,unitPrice: ""};
-  recette: any = {designation: "", recettePour: "", dateCreation: "", lignesRecettes: [{ consommation: "", matierepremier: {} }] };
-  ligneRecetteNumber:number =1;
-  ligneRecette: any = { consommation: "", matierepremier: {} };
+  units: any[] = [
+    { value: "NONE", label: "Aucun" },
+    { value: "METER", label: "Par mètre" },
+    { value: "KG", label: "En kilogramme" },
+    { value: "LITER", label: "En litre" },
+    { value: "PIECE", label: "Par piece" },
+    { value: "UNIT", label: "Par Unité" },
+  ];
+  produit: any = {
+    name: "",
+    stocklm: "",
+    SelectedFamily: {},
+    unit: "",
+    unitPrice: "",
+  };
+  recette: any = {
+    designation: "",
+    recettePour: "",
+    dateCreation: "",
+    lignesRecettes: [],
+  };
   evaIcons = [];
   chevronR = "chevron-right";
   chevronL = "chevron-left";
-  constructor(iconsLibrary: NbIconLibraries) {
-    this.evaIcons = Array.from(iconsLibrary.getPack('eva').icons.keys())
-    .filter(icon => icon.indexOf('outline') === -1);
+  formTitle: String = "";
+  @Input() produitToEdit: any;
+  @Input() status: String;
+  constructor(iconsLibrary: NbIconLibraries, private formBuilder: FormBuilder) {
+    this.evaIcons = Array.from(iconsLibrary.getPack("eva").icons.keys()).filter(
+      (icon) => icon.indexOf("outline") === -1
+    );
 
-    iconsLibrary.registerFontPack('ion', { iconClassPrefix: 'ion' });
+    iconsLibrary.registerFontPack("ion", { iconClassPrefix: "ion" });
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes.produitToEdit);
+    if (changes.produitToEdit.currentValue.status === "add") {
+      this.formTitle = "ajouter des nouvelles produits";
+    }
+
+    if (changes.produitToEdit.currentValue.status === "edit") {
+      this.formTitle =
+        "Editer le produit : " + changes.produitToEdit.currentValue.name;
+      this.produit.name = changes.produitToEdit.currentValue.name;
+      this.produit.stocklm = changes.produitToEdit.currentValue.stockLimit;
+      this.produit.SelectedFamily = this.familles.filter(
+        (fam) => fam.id === changes.produitToEdit.currentValue.famille.id
+      )[0];
+      this.produit.unit = changes.produitToEdit.currentValue.unit;
+      this.produit.unitPrice = changes.produitToEdit.currentValue.unitPrice;
+    }
+  }
+  ngOnInit(): void {
+    this.ligneRecetteForm = this.formBuilder.group({
+      lignesRecettes: this.formBuilder.array([this.newRecetteLineBuilder()]),
+    });
+    console.log("controls", this.lignesRecettes.controls);
   }
 
-  ngOnInit(): void {}
+  newRecetteLineBuilder(): FormGroup {
+    return this.formBuilder.group({
+      matierepremier: [null, Validators.required],
+      consommation: [null, Validators.required],
+    });
+  }
+  get lignesRecettes() {
+    return this.ligneRecetteForm.get("lignesRecettes") as FormArray;
+  }
+  addNewLineHandler() {
+    this.lignesRecettes.push(this.newRecetteLineBuilder());
+  }
+  removeLineHandler() {
+    this.lignesRecettes.removeAt(this.lignesRecettes.length - 1);
+  }
   addHandler() {
     console.log("Forms", this.produit);
+    console.log("Form lignes recettes", this.ligneRecetteForm.value);
+    console.log("recettes", this.recette);
   }
-  addNewLineHandler(){
-this.recette.lignesRecettes.push(this.ligneRecette);
-console.log(this.recette.lignesRecettes)
-  }
-  removeLineHandler(){
-    this.recette.lignesRecettes.splice(-1,1)
-    console.log(this.recette.lignesRecettes)
-  }
-
 }
